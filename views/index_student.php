36b810d76fb6b1be1ae1f17c90a4388647d52484
arbenamusa <43915605+arbenamusa@users.noreply.github.com>
2019-05-15 13:13:17 +0200

    index_pages fixed

--- views/index_student.php
+++ views/index_student.php
@@ -5,13 +5,22 @@
         <title>StuDB-Front Page</title>
         <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
         <link rel="stylesheet" href="/static/css/index.css">
+        <style>
+            #header-featured
+            {
+                height: 70%;
+                background-image: url(/static/img/pano_student.jpg);
+                background-position: center;
+                background-size: cover;
+            }
+        </style>
     </head>
     <body>
         <div id="header-wrapper">
             <div id="header" class="container">
                 <div id="logo">
-                    <h1><a href="aaa.html"></a>StuDB</a></h1>
-                    <img src="StuDB.png" width="55" height="55">
+                    <h1><a href="#"></a>StuDB</a></h1>
+                    <img src="/static/img/StuDB.png" width="55" height="55">
                 </div>
                 <div id="menu">
                     <ul>
@@ -22,10 +31,10 @@
                         <li><a href="#" title="">bllah 4</a></li>
                         <li>
                             <div class="dropdown">
-                                <img src="StuDB.png" alt="Avatar" width="50" height="50" style="border: #FF4B2B solid 2px;">
+                                <img src="/static/img/avatar.png" alt="Avatar" width="50" height="50" style="border: #FF4B2B solid 2px;">
                                 <div class="dropdown-content">
-                                    <img src="aaa.jpg" alt="Avatar" width="200" height="200" style="border: #FF4B2B solid 2px;">
-                                    <div class="personal">Emer Mbiemer</div>
+                                    <img src="/static/img/avatar.png" alt="Avatar" width="200" height="200" style="border: #FF4B2B solid 2px;">
+                                    <div class="personal"><?= $app->user->name() ?></div>
                                     <a href="#">Settings</a>
                                     <a href="#">Settings</a>
                                     <a href="#">Settings</a>
