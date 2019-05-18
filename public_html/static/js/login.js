237604a36cb70dcc160ae44d1a56b417f1d04eb8
ArditBaloku <arditbaloku@gmail.com>
2019-05-18 22:51:17 +0200

    Fixed logic error

--- public_html/static/js/login.js
+++ public_html/static/js/login.js
@@ -2,10 +2,23 @@ const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
+function clearErrors() {
+  var nodes = document.querySelectorAll('div.error');
+  var len = nodes.length;
+  for (var i = 0; i < len; i++) {
+    var node = nodes[i];
+    while (node.firstChild) {
+      node.removeChild(node.firstChild);
+    }
+  }
+}
+
 signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
+  clearErrors();
+  container.classList.add("right-panel-active");
 });
 
 signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+  clearErrors();
+  container.classList.remove("right-panel-active");
 });
