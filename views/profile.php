caf242b2c5e1af72a2cff891aecb751d3d444e50
anzotika <43915852+anzotika@users.noreply.github.com>
2019-05-21 23:26:25 +0200

    profile_view

---
+++ views/profile.php
@@ -0,0 +1,195 @@
+<html>
+<head>
+<title>
+</title>
+<link rel="stylesheet" href=" /static/css/profile.css"/>
+</head>
+<body>
+  <div class="resume-wrapper">
+  <article class="paper">
+<header>
+        <div class="header-content">
+          <div class="header-pic" title="picture">
+            <div class="pic"></div>
+          </div>
+          <div class="header-text">
+            <p>
+              <span class="first-name">Emri dhe Mbiemri</span>
+            </p>
+            <p class="subtitle">Niveli i arsimimit</p>
+          </div>
+        </div>
+      </header>
+      <div class="content-wrapper">
+        <section class="content">
+          <div class="row">
+            <div class="content-cat">
+             Contact
+            </div>
+            <div class="content-text">
+              <ul>
+                <li>Website</li>
+                <li>website</li>
+              </ul>
+              <ul>
+                <li>Email</li>
+                <li>email@gmail.com</li>
+              </ul>
+              <ul>
+                <li>Phone</li>
+                <li>Numri</li>
+              </ul>
+            </div>
+          </div>
+        </section>
+        <section class="content">
+          <div class="row">
+            <div class="content-cat">
+             Location
+            </div>
+            <div class="content-text">
+              <ul>
+                <li>Address</li>
+                <li>adresa</li>
+              </ul>
+              <ul>
+                <li>Postal Code</li>
+                <li>kodi postal (12135684)</li>
+              </ul>
+              <ul>
+                <li>City</li>
+                <li>qyteti</li>
+              </ul>
+            </div>
+          </div>
+        </section>
+        <section class="content">
+          <div class="row">
+            <div class="content-cat">
+             Profiles
+            </div>
+            <div class="content-text">
+              <ul>
+                <li>Twitter</li>
+                <li>
+                  <a href="#" target="_blank">twitter</a>
+                </li>
+              </ul>
+              <ul>
+                <li>Codepen</li>
+                <li>
+                  <a href="#" target="_blank">facebook</a>
+                </li>
+              </ul>
+              <ul>
+                <li>Github</li>
+                <li>
+                  <a href="#" target="_blank">LinkedIn</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+        <section class="content">
+          <div class="row">
+            <div class="content-cat big-text">
+             Shkollimi
+              <p>Viti</p>
+            </div>
+            <div class="content-text work-listing">
+              <p>Shkolla fillore/mesme</p>
+            </div>
+          </div>
+        </section>
+        <section class="content">
+          <div class="row">
+            <div class="content-cat">
+              EDUCATION
+              <p>From</p>
+              <p style="margin-top:4px;">-End</p>
+            </div>
+            <div class="content-text work-listing education-listing">
+              <p class="heading">****************</p>
+            </div>
+          </div>
+        </section>
+        <section class="content">
+          <div class="row">
+            <div class="content-cat big-text">
+              Professional career
+              <p style="margin-top:4px;">2013-07-13</p>
+            </div>
+            <div class="content-text work-listing education-listing">
+              <p class="heading">Faculty of Engineering and Computer Science at UP</p>
+              <p>ni paragraf</p>
+            </div>
+          </div>
+        </section>
+        <section class="content">
+          <div class="row">
+            <div class="content-cat big-text">
+              UNIVERSITY CAREER
+              <p style="margin-top:4px;">Since</p>
+            </div>
+            <div class="content-text work-listing education-listing">
+              <p class="heading">AUK,UP,(KOLEGJI AAB LOL)</p>
+              <p>ni paragraf</p>
+            </div>
+          </div>
+        </section>
+        <section class="content">
+          <div class="row">
+            <div class="content-cat big-text">
+              SCIENTIFIC PUBLICATIONS
+            </div>
+            <div class="content-text work-listing education-listing">
+              <p class="heading">PUBLIKIMI</p>
+              <p>ni paragraf</p>
+            </div>
+          </div>
+        </section>
+        <section class="content">
+          <div class="row">
+            <div class="content-cat big-text">
+              INVITED SPEAKER
+            </div>
+            <div class="content-text work-listing education-listing">
+              <p class="heading">“The role of education in security/privacy awareness raising”</p>
+              <p>ni paragraf</p>
+            </div>
+          </div>
+        </section>
+        <section class="content">
+          <div class="row">
+            <div class="content-cat big-text">
+              Skills
+            </div>
+            <div class="content-text skills-listing">
+              <ul>
+                <li>
+                  Web
+                  <span>(Master)</span>
+                </li>
+                <li>
+                  Mobile
+                  <span>(Advanced)</span>
+                </li>
+              </ul>
+              <ul>
+                <li>
+                  Languages
+                  <span>(Master)</span>
+                </li>
+                <li>
+                  Software
+                  <span>(Advanced)</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </div>
+    </article>
+  </div>
+</body>
+</html>
