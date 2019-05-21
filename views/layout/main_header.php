59752564c394b45d146b47bbd834073b92e5413a
arbenamusa <43915605+arbenamusa@users.noreply.github.com>
2019-05-21 23:34:21 +0200

    fixed profile view link

--- views/layout/main_header.php
+++ views/layout/main_header.php
@@ -36,7 +36,7 @@
                 <div class="dropdown-content">
                   <img src="/static/img/avatar.png" alt="Avatar" width="200" height="200" style="border: #FF4B2B solid 2px;">
                   <div class="personal"><?= $app->user->name() ?></div>
-                    <a href="../views/profile.php">Profili</a>
+                    <a href="/profile.php">Profili</a>
                     <a href="/logout.php"><button class="button1">Sign out</button></a></div>
                   </div>
                 </div>
