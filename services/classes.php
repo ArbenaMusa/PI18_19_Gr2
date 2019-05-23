723d50a474d67ed479b94580dddb7f2657d0df5e
ArditBaloku <arditbaloku@gmail.com>
2019-05-23 16:30:15 +0200

    added viewList in classes.php

--- services/classes.php
+++ services/classes.php
@@ -2,12 +2,17 @@
 
 interface IClassManager {
   public function create($data);
+  public function view();
 }
 
 class SqlClassManager implements IClassManager {
-  public function create($data) {
-    $db = $this->app->db;
 
+  public function __construct() {
+    $db = $this->app->db;
+  }
+
+  //TODO create
+  public function create($data) {
     $query1 = <<< SQL
     INSERT INTO class(classname, semester, no_of_groups)
     VALUES(%s, %s, %d)
@@ -23,13 +28,24 @@ SQL;
       return true;
     }
 
-    $teacherId = $db->first("SELECT id FROM users WHERE name = %s", $data->teacher);
     $classId = $db->first("SELECT id FROM classes WHERE name = %s", $data->classname);
 
-    if(!$db->execute($query2, $teacherId, $classId)) {
+    if(!$db->execute($query2, $app->user->id(), $classId)) {
       return true;
     }
     return false;
+  }
+
+  public function viewList() {
+    $query = <<<SQL
+    SELECT classname
+    FROM classes cls
+    INNER JOIN teaches t
+    ON cls.id = t.classId
+    WHERE t.teacherId = %s
+SQL;
+
+    return $db->query($query, $app->user->id());
   }
 }
 
