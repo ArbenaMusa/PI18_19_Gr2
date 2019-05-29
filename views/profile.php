0af7c77c22a5b51e2cb26032f2f32ba7e4fb0c30
eduardspahija <eduard-spahija@hotmail.com>
2019-05-29 23:07:40 +0200

    Update profile.php
    Fshirje e informatave te panevojshme ne profilin e perdoruesve

--- views/profile.php
+++ views/profile.php
@@ -2,6 +2,7 @@
   'css' => '/static/css/profile.css',
   'title' => $app->user->name() . "'s profile"
 ]) ?>
+
 <body>
   <div class="resume-wrapper">
   <article class="paper">
@@ -12,7 +13,7 @@
           </div>
           <div class="header-text">
             <p>
-              <span class="first-name">Emri dhe Mbiemri</span>
+              <span class="first-name"><?= $app->user->name() ?></span>
             </p>
             <p class="subtitle">Niveli i arsimimit</p>
           </div>
@@ -27,36 +28,15 @@
             <div class="content-text">
               <ul>
                 <li>Website</li>
-                <li>website</li>
+                <li><?= $app->user->website() ?></li>
               </ul>
               <ul>
                 <li>Email</li>
-                <li>email@gmail.com</li>
+                <li><?= $app->user->email() ?></li>
               </ul>
               <ul>
                 <li>Phone</li>
-                <li>Numri</li>
-              </ul>
-            </div>
-          </div>
-        </section>
-        <section class="content">
-          <div class="row">
-            <div class="content-cat">
-             Location
-            </div>
-            <div class="content-text">
-              <ul>
-                <li>Address</li>
-                <li>adresa</li>
-              </ul>
-              <ul>
-                <li>Postal Code</li>
-                <li>kodi postal (12135684)</li>
-              </ul>
-              <ul>
-                <li>City</li>
-                <li>qyteti</li>
+                <li><?= $app->user->phone() ?></li>
               </ul>
             </div>
           </div>
@@ -74,13 +54,13 @@
                 </li>
               </ul>
               <ul>
-                <li>Codepen</li>
+                <li>Facebook</li>
                 <li>
                   <a href="#" target="_blank">facebook</a>
                 </li>
               </ul>
               <ul>
-                <li>Github</li>
+                <li>LinkedIn</li>
                 <li>
                   <a href="#" target="_blank">LinkedIn</a>
                 </li>
@@ -88,104 +68,7 @@
             </div>
           </div>
         </section>
-        <section class="content">
-          <div class="row">
-            <div class="content-cat big-text">
-             Shkollimi
-              <p>Viti</p>
-            </div>
-            <div class="content-text work-listing">
-              <p>Shkolla fillore/mesme</p>
-            </div>
-          </div>
-        </section>
-        <section class="content">
-          <div class="row">
-            <div class="content-cat">
-              EDUCATION
-              <p>From</p>
-              <p style="margin-top:4px;">-End</p>
-            </div>
-            <div class="content-text work-listing education-listing">
-              <p class="heading">****************</p>
-            </div>
-          </div>
-        </section>
-        <section class="content">
-          <div class="row">
-            <div class="content-cat big-text">
-              Professional career
-              <p style="margin-top:4px;">2013-07-13</p>
-            </div>
-            <div class="content-text work-listing education-listing">
-              <p class="heading">Faculty of Engineering and Computer Science at UP</p>
-              <p>ni paragraf</p>
-            </div>
-          </div>
-        </section>
-        <section class="content">
-          <div class="row">
-            <div class="content-cat big-text">
-              UNIVERSITY CAREER
-              <p style="margin-top:4px;">Since</p>
-            </div>
-            <div class="content-text work-listing education-listing">
-              <p class="heading">AUK,UP,(KOLEGJI AAB LOL)</p>
-              <p>ni paragraf</p>
-            </div>
-          </div>
-        </section>
-        <section class="content">
-          <div class="row">
-            <div class="content-cat big-text">
-              SCIENTIFIC PUBLICATIONS
-            </div>
-            <div class="content-text work-listing education-listing">
-              <p class="heading">PUBLIKIMI</p>
-              <p>ni paragraf</p>
-            </div>
-          </div>
-        </section>
-        <section class="content">
-          <div class="row">
-            <div class="content-cat big-text">
-              INVITED SPEAKER
-            </div>
-            <div class="content-text work-listing education-listing">
-              <p class="heading">“The role of education in security/privacy awareness raising”</p>
-              <p>ni paragraf</p>
-            </div>
-          </div>
-        </section>
-        <section class="content">
-          <div class="row">
-            <div class="content-cat big-text">
-              Skills
-            </div>
-            <div class="content-text skills-listing">
-              <ul>
-                <li>
-                  Web
-                  <span>(Master)</span>
-                </li>
-                <li>
-                  Mobile
-                  <span>(Advanced)</span>
-                </li>
-              </ul>
-              <ul>
-                <li>
-                  Languages
-                  <span>(Master)</span>
-                </li>
-                <li>
-                  Software
-                  <span>(Advanced)</span>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </section>
+
       </div>
     </article>
   </div>
